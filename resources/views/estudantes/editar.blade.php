812d52e1bde013a78d2ab68c1da8397bb24a7c75
Cristiano Garcia <cristianooo@gmail.com>
2025-03-31 16:21:51 -0300

    controllers ok, model ok, migration ok, view ok

---
+++ resources/views/estudantes/editar.blade.php
@@ -0,0 +1,44 @@
+@extends('template')
+
+@section('conteudo')
+<h1>Estudante <b>{{ $estudante->nome }}</b></h1>
+
+<form method="post" action="{{ route('estudantes.salvar', [ 'id' => $estudante->id ]) }}">
+    @csrf
+    <div class="row">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="nome" name="nome" class="form-control" id="nome" placeholder="Nome do(a) estudante"
+            value="{{ $estudante->nome }}">
+        </div>
+        <div class="mb-3">
+            <label for="matricula" class="form-label">Matrícula</label>
+            <input type="matricula" name="matricula" class="form-control" id="matricula" placeholder="Matricula do(a) estudante"
+            value="{{ $estudante->matricula }}">
+        </div>
+    </div>
+    <hr>
+    <input type="submit" value="Salvar mudanças" class="btn btn-success" />
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalExcluir">
+        Excluir
+    </button>
+</form>
+
+<div class="modal" tabindex="-1" id="modalExcluir">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Exclusão</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Deseja realmente excluir o(a) estudante <b>{{ $estudante->nome }}</b>?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <a href="{{ route('estudantes.excluir', ['id' => $estudante->id]) }}" class="btn btn-outline-danger">Excluir</a>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
