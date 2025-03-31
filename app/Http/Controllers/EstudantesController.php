72a11e11d5e4dd8e0f7c6002ec4da0037a5704e6
Cristiano Garcia <cristianooo@gmail.com>
2025-03-31 16:09:35 -0300

    controllers ok, model ok, migration ok

---
+++ app/Http/Controllers/EstudantesController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Estudante;
+
+class EstudantesController extends Controller
+{
+    function show(){
+        $estudantes = Modulo::all();
+
+        return view('estudantes.show', ['estudantes' => $estudantes]);
+    }
+
+    function novo(){
+
+        return view('estudantes.novo');
+    }
+
+    function editar($id){
+        $estudante = Estudante::findOrFail($id);
+
+        return view('estudantes.editar', ['estudante' => $estudante]);
+    }
+
+    function excluir($id){
+        $estudante = Estudante::findOrFail($id);
+        
+        $nome = $estudante->nome;
+        $estudante->delete();
+
+        session()->flash('mensagem', "Módulo {$nome} excluída com sucesso.");
+        
+        return redirect()->route('estudantes.show');
+    }
+
+    function salvar(Request $req, $id=null){
+        if ($id == null){
+            // Novo
+            $estudante = new Estudante();
+            $operacao = "inserido";
+        } else { 
+            // Alteração
+            $estudante = Estudante::findOrFail($id);
+            $operacao = "alterado";
+        }     
+        $estudante->nome = $req->nome;
+        $estudante->disciplina_id = $req->disciplina_id;
+
+        $estudante->save();
+
+        session()->flash('mensagem', "Estudante {$estudante->nome} {$operacao} com sucesso.");
+        
+        return redirect()->route('estudantes.show');
+    }
+}
