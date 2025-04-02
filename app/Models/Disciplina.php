318eb9ac8ee621ed7a2820ba5fda73b30e21e241
Cristiano Garcia <cristianooo@gmail.com>
2025-04-02 12:20:57 -0300

    Ajuste de ModulosController

--- app/Models/Disciplina.php
+++ app/Models/Disciplina.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+use App\Models\Modulo;
 
 class Disciplina extends Model
 {
-    //
+    function modulos(){
+        return $this->hasMany(Modulo::class);
+    }
 }
--- app/Models/Modulo.php
+++ app/Models/Modulo.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\BelongsTo;
+
+use App\Models\Disciplina;
 
 class Modulo extends Model
 {
-    //
+    function disciplina(){
+        return $this->belongsTo(Disciplina::class);
+    }
 }
